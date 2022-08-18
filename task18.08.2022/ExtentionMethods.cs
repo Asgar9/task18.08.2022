f5eb378e20e674e1fb83d43c6b394169b0a0fb61
Asgar9 <asgaryas@code.edu.az>
2022-08-18 12:13:45 +0400

    add edildi

---
+++ task18.08.2022/ExtentionMethods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task18._08._2022
+{
+    class ExtentionMethods
+    {
+        public static bool IsOdd(int dig)
+        {
+            if (dig%2==0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool IsEven(int dig)
+        {
+            if (dig % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsContainsDigit(string strr)
+        {
+            foreach (char item in strr)
+            {
+                if (char.IsDigit() == item)
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+        public static string ToCapitalize(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(char.ToUpper(str[0]));
+            sb.Append(str.Substring(1).ToLower());
+            return sb.ToString();
+
+        }
+
+
+    }
+}
---
+++ task18.08.2022/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task18._08._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("texti daxil edin:");
+            string str = Console.ReadLine();
+           Console.WriteLine(ExtentionMethods.ToCapitalize(str));
+            Console.WriteLine("Include the digit:");
+            int digit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ExtentionMethods.IsOdd(digit));
+            Console.WriteLine("Include the digit:");
+            int digit1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ExtentionMethods.IsEven(digit1));
+        } 
+
+    }
+}
